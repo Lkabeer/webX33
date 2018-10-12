81fb7d9dd1de576fdd36f0b2699cd5a4cce04780
Ahmed Samir <ahmedsamir@Ahmeds-MacBook-Pro.local>
2018-10-12 17:14:48 +0200

    version X0.1

---
+++ js/script.js
@@ -0,0 +1,5 @@
+AOS.init({
+    duration: 600, // values from 0 to 3000, with step 50ms
+    easing: 'ease-in-out-back', // default easing for AOS animations
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
